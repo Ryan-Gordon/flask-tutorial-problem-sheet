339d3821381a05437ef441ba93a6817ee8e613db
Ryan Gordon <ryangordon210@gmail.com>
2017-10-23 13:03:10 +0100

    Added http example

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Code adapted from http://flask.pocoo.org/docs/0.12/quickstart/#
 from flask import Flask
+import requests #Requests used to handle HTTP requests
+import json #Requests used to handle json manipulation 
 app = Flask(__name__)
 
 '''
@@ -12,10 +14,26 @@ def hello_world():
     return 'Hello, World!'
 
 
-
+'''
+Specifying a path as <path> allows you as the dev to take that part of the path as a variable
+'''
 @app.route('/<path>')
 def parrot_path(path):
     return 'You entered this path : '+ path
+
+'''
+A practical example of a http request to an api, which will return data.
+'''
+@app.route('/http/<path>')
+def http_example(path):
+    # Pull JSON market data from Poloniex
+    r = requests.get('https://api.coinmarketcap.com/v1/ticker/'+path+'/')
+    data = r.json() # Gather the json data
+   
+    return json.dumps(data)
+    # end function
+
+
 ''' Commands to run the flask app : 
 
 export FLASK_APP=hello.py
